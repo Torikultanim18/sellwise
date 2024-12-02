a88a18afee52b0d49dd5d68ea95041ff2c06ce09
Torikultanim18 <tanimbd081@gmail.com>
2024-12-02 17:33:21 +0600

    home page's marquee testimonial , accordion and header and project page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,56 @@
-$(document).ready(function(){
+// Select the marquee containers
+const marqueeContainers = document.querySelectorAll('.testimonial-cards');
+
+marqueeContainers.forEach((container) => {
+  const childNodes = Array.from(container.children);
+
+  // Duplicate the nodes and append them
+  childNodes.forEach((node) => {
+    const clone = node.cloneNode(true);
+    container.appendChild(clone);
+  });
+});
 
 
-    const mobilenav = () => {
-        let wrapper = document.querySelector('.navrapper')
+// accordion functionalities
 
-        if(wrapper){
+// Get all accordion buttons and items
+const accordionButtons = document.getElementsByClassName("accordion-button");
 
-            // Code goes here
-        }
+// Loop through all accordion buttons
+for (let i = 0; i < accordionButtons.length; i++) {
+  // Add event listener for button click
+  accordionButtons[i].addEventListener("click", function (event) {
+    const icon = event.currentTarget.querySelector(".plus-icon");
+
+    // Toggle between plus and minus icon
+    if (icon.src.includes("plusicon.svg")) {
+      icon.src = "./assets/images/icons/minusicon.svg";
+    } else {
+      icon.src = "./assets/images/icons/plusicon.svg";
     }
+  });
+}
 
-    mobilenav()
-    
-    const footerLink = () => {
-        let wrapper = document.querySelector('.footer')
-
-        if(wrapper){
-
-            // Code goes here
-
-        }
+document.querySelectorAll(".accordion-collapse").forEach(function (collapse) {
+  collapse.addEventListener("shown.bs.collapse", function () {
+    let button = this.previousElementSibling.querySelector(".accordion-button");
+    if (button) {
+      let icon = button.querySelector(".plus-icon");
+      if (icon) {
+        icon.src = "./assets/images/icons/minus-icon.svg";
+      }
     }
+  });
 
-    footerLink()
-
-   
-    
-
-})
+  collapse.addEventListener("hidden.bs.collapse", function () {
+    const button =
+      this.previousElementSibling.querySelector(".accordion-button");
+    if (button) {
+      const icon = button.querySelector(".plus-icon");
+      if (icon) {
+        icon.src = "./assets/images/icons/plus-icon.svg";
+      }
+    }
+  });
+});
